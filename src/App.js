8ae4dc47fe6ea79ec4b0ad761e07978a390eea74
Andrew Noviello <andnov24@yahoo.com>
2024-07-12 08:47:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Contact from './components/Contact';
+import Recruitment from './components/Recruitment';
+import Alumni from './components/Alumni';
+import Team from './components/Team';
+
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+  </>
+);
+
+const Home = () => (
+  <>
+    <Hero />
+    <About />
+    <Contact />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/recruitment', element: <Recruitment /> },
+      { path: '/alumni', element: <Alumni /> },
+      { path: '/team', element: <Team /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
